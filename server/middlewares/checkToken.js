4a2df6ccc3e6dd22b4e3e2036ce05a360213cbc0
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2025-04-11 01:59:01 +0200

    ajout de la vérification du token dans le middleware avec gestion des erreurs pour token manquant et invalide

--- server/middlewares/checkToken.js
+++ server/middlewares/checkToken.js
@@ -2,13 +2,18 @@ import jwt from 'jsonwebtoken'
 
 const checkToken = (req, res, next) => {
     const header = req.headers['authorization']
+
+    if (!header || !header.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Accès refusé: Token invalide' });
+    }
+
     const token = header && header.split(' ')[1]
 
     if (!token) {
         return res.status(401).json({ message: 'Accès refusé: Token manquant' })
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
         if (err) {
             return res.status(403).json({ message: 'Token invalide ou expiré' })
         }
