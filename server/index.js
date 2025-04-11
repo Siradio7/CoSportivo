2b0caf65fefe80cb2f646056d9807bb4eb6857de
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2025-04-11 05:40:49 +0200

    ajout de la prise en charge de Socket.IO pour la gestion des messages en temps réel dans les discussions

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import { Server } from "socket.io"
 import authRoutes from "./routes/auth.js"
 import checkToken from "./middlewares/checkToken.js"
 import footballDataRoutes from "./routes/footballData.js"
 import usersRoutes from "./routes/users.js"
 import sanitizeInput from "./middlewares/sanitizeInput.js"
+import setupChat from "./socket/chat.js"
 
 dotenv.config()
 
@@ -19,6 +21,16 @@ app.use("/auth", authRoutes)
 app.use("/api", checkToken, footballDataRoutes)
 app.use("/users", checkToken, sanitizeInput, usersRoutes)
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Serveur en écoute sur le port ${PORT}`)
 })
+
+const io = new Server(server, {
+    cors: {
+        origin: process.env.DEV_FRONTEND_URL,
+        methods: ["GET", "POST"],
+    }
+})
+
+setupChat(io)
+io.listen(4000)
---
+++ server/socket/chat.js
@@ -0,0 +1,36 @@
+import { dbQuery } from "../utils/dbQuery.js"
+
+const setupChat = (io) => {
+    io.on('connection', (socket) => {
+
+        socket.on("joinTrip", async ({ tripId }) => {
+            try {
+                const messages = await dbQuery("SELECT * FROM messages WHERE trip_id = ?", [tripId])
+
+                socket.emit("loadMessages", messages)
+                socket.join(tripId)
+            } catch (err) {
+                socket.emit("error", { message: "Erreur lors de la récupération des messages" })
+                console.error("Erreur récupération messages:", err)
+            }
+        })
+
+        socket.on("sendMessage", async ({ message, tripId, user }) => {
+            try {
+                const sql = "INSERT INTO messages (trip_id, user_id, username, message) VALUES (?, ?, ?, ?)"
+
+                await dbQuery(sql, [tripId, user.id, user.first_name, message])
+                io.to(tripId).emit("receiveMessage", {
+                    message,
+                    user,
+                    time: new Date()
+                })
+            } catch (err) {
+                socket.emit("error", { message: "Erreur lors de l'envoi du message" })
+                console.error("Erreur envoi message:", err)
+            }
+        })
+    })
+}
+
+export default setupChat
