6092f9384ae6ecf7d4138e2b6adcac43627f6d75
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2025-04-12 21:21:20 +0200

    ajout de la gestion des routes avec un composant PrivateRoute et intégration de la page de connexion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Toaster } from "react-hot-toast"
 import Home from "./pages/Home"
+import Login from "./pages/Login"
+import PrivateRoute from "./utils/privateRoute"
+import Dashboard from "./pages/Dashboard"
 
 const App = () => {
     return (
         <Router>
+            <Toaster
+                position="bottom-right"
+                toastOptions={{
+                    duration: 3000
+                }}
+            />
+            
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route
+                    path="/dashboard"
+                    element={<PrivateRoute element={<Dashboard />} />}
+                />
             </Routes>
         </Router>
     )
